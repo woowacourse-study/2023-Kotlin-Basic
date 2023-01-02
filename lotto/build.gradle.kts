226f388f4e56e2ed10f35c5f82f1e02807293708
injoon2019 <ison@sfu.ca>
2023-01-02 22:41:18 +0900

    init: 로또 미션

---
+++ lotto/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.6.20"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+group = "camp.nextstep.edu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
+    testImplementation("org.assertj", "assertj-core", "3.22.0")
+    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+    ktlint {
+        verbose.set(true)
+    }
+}
