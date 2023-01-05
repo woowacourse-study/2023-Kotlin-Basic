691c8b47f48c507ca3f658d47f72c3157798bcdd
devHudi <devhudi@gmail.com>
2023-01-05 23:41:32 +0900

    feat: 당첨 등수 계산 로직 리팩토링

--- lotto/src/main/kotlin/domain/winning/Rank.kt
+++ lotto/src/main/kotlin/domain/winning/Rank.kt
@@ -5,33 +5,29 @@ import domain.money.Money
 
 enum class Rank(
     private val matchCount: Int?,
-    private val includeBonus: Boolean?,
-    private val money: Money?
+    private val includeBonus: Boolean,
+    private val money: Money
 ) {
     FIRST(6, false, Money(2_000_000_000)),
     SECOND(6, true, Money(30_000_000)),
     THIRD(5, true, Money(1_500_000)),
     FOURTH(4, true, Money(50_000)),
     FIFTH(3, true, Money(5_000)),
-    NONE(null, null, null),
+    NONE(null, false, Money(0)),
     ;
 
     // TODO: refactor
     companion object {
         fun match(lotto: Lotto, winningLotto: WinningLotto): Rank {
-            return if (lotto.match(winningLotto.winningLotto) == 6) {
-                FIRST
-            } else if (lotto.match(winningLotto.winningLotto) == 5 && lotto.has(winningLotto.bonusNumber)) {
-                SECOND
-            } else if (lotto.match(winningLotto.winningLotto) == 5) {
-                THIRD
-            } else if (lotto.match(winningLotto.winningLotto) == 4) {
-                FOURTH
-            } else if (lotto.match(winningLotto.winningLotto) == 3) {
-                FIFTH
-            } else {
-                NONE
-            }
+            return values().find { rank ->
+                var matchCount = lotto.match(winningLotto.winningLotto)
+                val hasBonus = lotto.has(winningLotto.bonusNumber)
+                if (rank.includeBonus && hasBonus) {
+                    matchCount++
+                }
+
+                rank.matchCount == matchCount
+            } ?: NONE
         }
     }
 }
