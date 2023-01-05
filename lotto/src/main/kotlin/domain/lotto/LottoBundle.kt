afba1854e34b8df08c01b9d8a4900fd37ee6a256
devHudi <devhudi@gmail.com>
2023-01-06 01:53:30 +0900

    feat: 구매한 모든 로또에 대한 일급 컬렉션 구현

---
+++ lotto/src/main/kotlin/domain/lotto/LottoBundle.kt
@@ -0,0 +1,20 @@
+package domain.lotto
+
+import domain.money.Money
+
+class LottoBundle(
+    inputMoney: Money,
+    manualLottos: List<Lotto>
+) {
+    val lottos: List<Lotto>
+
+    init {
+        val lottoCount: Int = inputMoney.value / Lotto.LOTTO_PRICE
+        val autoCount = lottoCount - manualLottos.size
+
+        val autoLottos = (1..autoCount).toList()
+            .map { Lotto.auto() }
+
+        lottos = manualLottos + autoLottos
+    }
+}
