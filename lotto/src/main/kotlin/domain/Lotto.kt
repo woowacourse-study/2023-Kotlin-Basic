214c1f6d731773df4ac68b021605a9a690eae636
hyewoncc <hyewoncc@gmail.com>
2023-01-10 22:34:35 +0900

    refactor: 로또 내 숫자 컬렉션 자료구조를 Set으로 변경

--- lotto/src/main/kotlin/domain/Lotto.kt
+++ lotto/src/main/kotlin/domain/Lotto.kt
@@ -1,7 +1,6 @@
 package domain
 
 class WinningLotto(private val lotto: Lotto, private val bonus: Ball) {
-
     init {
         require(!lotto.contains(bonus)) { "보너스 숫자는 로또 번호와 중복될 수 없습니다" }
     }
@@ -14,11 +13,10 @@ class WinningLotto(private val lotto: Lotto, private val bonus: Ball) {
 }
 
 class Lotto(strategy: LottoStrategy) {
-
-    private val balls: List<Ball>
+    private val balls: Set<Ball>
 
     init {
-        balls = strategy.generateNumbers().map { Ball(it) }
+        balls = strategy.generateNumbers().map { Ball(it) }.toSet()
         require(balls.distinct().size == 6) { "로또는 중복되지 않는 6개 숫자로 구성되어야 합니다" }
     }
 
@@ -32,7 +30,6 @@ class Lotto(strategy: LottoStrategy) {
 }
 
 data class Ball(val number: Int) {
-
     init {
         require(number in 1..45) { "로또 숫자는 1 이상 45 이하입니다" }
     }
