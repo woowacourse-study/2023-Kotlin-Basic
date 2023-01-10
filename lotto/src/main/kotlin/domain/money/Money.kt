bddef4f3eaeb191701f7942d2e446b628882f666
devHudi <devhudi@gmail.com>
2023-01-10 16:46:40 +0900

    refactor: companion object를 최하단으로 이동

--- lotto/src/main/kotlin/domain/money/Money.kt
+++ lotto/src/main/kotlin/domain/money/Money.kt
@@ -3,10 +3,6 @@ package domain.money
 data class Money(
     val value: Int
 ) {
-    companion object {
-        private const val MONEY_MIN_UNIT = 1_000
-    }
-
     init {
         require(value >= 0) { "금액은 음수일 수 없습니다." }
         require(isMultipleOfMinUnit()) { "금액은 1,000원 단위여야 합니다." }
@@ -14,5 +10,9 @@ data class Money(
 
     private fun isMultipleOfMinUnit(): Boolean {
         return value % MONEY_MIN_UNIT == 0
+    }
+
+    companion object {
+        private const val MONEY_MIN_UNIT = 1_000
     }
 }
