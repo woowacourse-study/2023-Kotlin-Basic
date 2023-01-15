4de801788e958b96347d83dc29bc6d73716d36ed
Ohzzi <jihoon970712@gmail.com>
2023-01-15 22:31:05 +0900

    refactor: subList를 take로 변경

--- lotto/src/main/kotlin/domain/LottoMachine.kt
+++ lotto/src/main/kotlin/domain/LottoMachine.kt
@@ -5,5 +5,5 @@ object LottoMachine {
 
     fun issueAutomatically(count: PurchaseCount): List<Lotto> = (1..count.value).map { issueRandomLotto() }
 
-    private fun issueRandomLotto(): Lotto = Lotto.from(lottoNumberPool.shuffled().subList(0, Lotto.NUMBERS_SIZE))
+    private fun issueRandomLotto(): Lotto = Lotto.from(lottoNumberPool.shuffled().take(Lotto.NUMBERS_SIZE))
 }
