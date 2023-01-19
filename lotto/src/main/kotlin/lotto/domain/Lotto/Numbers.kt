f0bd87e83b8761e55fc5a7bd486f2d973c19e2ed
Ohzzi <jihoon970712@gmail.com>
2023-01-20 00:23:59 +0900

    Revert "[토르] 코틀린 로또 미션 제출합니다 (#1)"
    This reverts commit a4bfb68777786604df556dcaa73bf9829a7c00f2.

--- lotto/src/main/kotlin/lotto/domain/Lotto/Numbers.kt
+++
@@ -1,9 +0,0 @@
-package lotto.domain.Lotto
-
-object Numbers {
-
-        private val values = (1..45)
-
-        fun getShuffledNumbers(count: Int): List<Int> =
-            values.shuffled().subList(0, count).toList()
-}
