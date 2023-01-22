a5672f9295391d4ef833c5744230e0603d2ae48a
Ohzzi <jihoon970712@gmail.com>
2023-01-22 21:49:22 +0900

    refactor: 메인 로직의 인덴트를 줄이도록 수정

--- wordle/src/main/kotlin/WordleApplication.kt
+++ wordle/src/main/kotlin/WordleApplication.kt
@@ -1,19 +1,14 @@
+import domain.JudgementTile
 import domain.PickAnswer
 import domain.Wordle
 import view.*
 
 fun main() {
     printStartMessage()
-
     val wordle = Wordle(PickAnswer.Default())
     while (!wordle.isEnd) {
         val judgementTiles = retryOnException { wordle.proceedRound(inputPrediction()) }
-        if (wordle.isEnd) {
-            printTryCount(wordle.round)
-            if (!judgementTiles.last().isAllGreen()) {
-                printFailMessage(wordle.answer)
-            }
-        }
+        terminateGameIfEnd(wordle, judgementTiles)
         printTiles(judgementTiles)
     }
 }
@@ -24,5 +19,18 @@ private fun <T> retryOnException(function: () -> T): T {
     } catch (e: Exception) {
         println(e.localizedMessage)
         retryOnException(function)
+    }
+}
+
+private fun terminateGameIfEnd(wordle: Wordle, judgementTiles: List<JudgementTile>) {
+    if (wordle.isEnd) {
+        printTryCount(wordle.round)
+        printIfFailed(judgementTiles, wordle)
+    }
+}
+
+private fun printIfFailed(judgementTiles: List<JudgementTile>, wordle: Wordle) {
+    if (!judgementTiles.last().isAllGreen()) {
+        printFailMessage(wordle.answer)
     }
 }
