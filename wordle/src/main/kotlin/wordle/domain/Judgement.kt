87b4bc899412c5f4b22775a7e4c7f61df02fc738
hyeonic <dev.hyeonic@gmail.com>
2023-01-23 16:58:02 +0900

    refactor: Judgement 가독성을 위한  메서드 분리

--- wordle/src/main/kotlin/wordle/domain/Judgement.kt
+++ wordle/src/main/kotlin/wordle/domain/Judgement.kt
@@ -26,30 +26,38 @@ class Judgement(
 
     private fun fillGreenTiles(tiles: MutableList<Tile>, countOfChars: MutableMap<Char, Int>) {
         for (index in 0 until answer.value.length) {
-            if (answer.isSameIndex(guess, index)) {
-                tiles[index] = Tile.GREEN
-                countOfChars.decreaseIfPresent(guess.value[index])
-            }
+            fillGreenTile(index, tiles, countOfChars)
+        }
+    }
+
+    private fun fillGreenTile(index: Int, tiles: MutableList<Tile>, countOfChars: MutableMap<Char, Int>) {
+        if (answer.isSameIndex(guess, index)) {
+            tiles[index] = Tile.GREEN
+            countOfChars.decreaseIfPresent(guess.value[index])
         }
     }
 
     private fun fillYellowTiles(tiles: MutableList<Tile>, countOfChars: MutableMap<Char, Int>) {
         for (index in 0 until answer.value.length) {
-            if (isFillYellowTile(tiles, index, countOfChars)) {
-                tiles[index] = Tile.YELLOW
-                countOfChars.decreaseIfPresent(guess.value[index])
-            }
+            fillYellowTile(tiles, index, countOfChars)
         }
     }
 
-    private fun MutableMap<Char, Int>.decreaseIfPresent(key: Char) {
-        computeIfPresent(key) { _, value -> value - 1 }
+    private fun fillYellowTile(tiles: MutableList<Tile>, index: Int, countOfChars: MutableMap<Char, Int>) {
+        if (isFillYellowTile(tiles, index, countOfChars)) {
+            tiles[index] = Tile.YELLOW
+            countOfChars.decreaseIfPresent(guess.value[index])
+        }
     }
 
     private fun isFillYellowTile(tiles: MutableList<Tile>, index: Int, countOfChars: MutableMap<Char, Int>) =
         tiles[index].isGray()
                 && countOfChars.containsKey(guess.value[index])
                 && countOfChars[guess.value[index]] != 0
+
+    private fun MutableMap<Char, Int>.decreaseIfPresent(key: Char) {
+        computeIfPresent(key) { _, value -> value - 1 }
+    }
 
     companion object {
         val DEFAULT_TILES = listOf(Tile.GRAY, Tile.GRAY, Tile.GRAY, Tile.GRAY, Tile.GRAY)
