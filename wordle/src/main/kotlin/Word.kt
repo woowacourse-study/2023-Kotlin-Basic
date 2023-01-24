d37da9937944778e15579415bc0cfd65087b6f6e
hyewoncc <hyewoncc@gmail.com>
2023-01-24 22:53:00 +0900

    feat: 단어 간 철자 비교 후 결과 계산

--- wordle/src/main/kotlin/Word.kt
+++ wordle/src/main/kotlin/Word.kt
@@ -1,4 +1,45 @@
-class Word(private val alphabets: List<Alphabet>) {
+class Word(word: String) {
+
+    private val alphabets: List<Alphabet> = word.map { Alphabet(it) }
+
+    init {
+        require(alphabets.size == 5) { "단어는 다섯글자로 구성됩니다" }
+    }
+
+    fun compare(other: Word): List<Compared> {
+        val result = MutableList(5) { Compared.NONE }
+        val pool = AlphabetsPool(alphabets)
+
+        for ((i, alphabet) in other.alphabets.withIndex()) {
+            if (alphabets[i] == alphabet) {
+                result[i] = Compared.EQUAL
+                pool.spend(alphabet)
+                continue
+            }
+
+            if (alphabets.contains(alphabet) && pool.has(alphabet)) {
+                result[i] = Compared.EXIST
+                pool.spend(alphabet)
+            }
+        }
+        return result
+    }
+}
+
+class AlphabetsPool(alphabets: List<Alphabet>) {
+
+    private val pool: MutableMap<Alphabet, Int> = alphabets.groupingBy { it }.eachCount().toMutableMap()
+
+    fun has(alphabet: Alphabet): Boolean {
+        val count = pool[alphabet] ?: return false
+        return count > 0
+    }
+
+    fun spend(alphabet: Alphabet) {
+        if (pool.containsKey(alphabet)) {
+            pool[alphabet] = pool[alphabet]!!.minus(1)
+        }
+    }
 }
 
 class Alphabet(private val value: Char) {
--- wordle/src/test/kotlin/WordTest.kt
+++ wordle/src/test/kotlin/WordTest.kt
@@ -1,10 +1,104 @@
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
 
 class WordTest: StringSpec({
 
+    "단어는 다섯글자로 구성해야 한다" {
+        listOf("java", "kotlin").forAll { invalidValue ->
+            shouldThrow<IllegalArgumentException> { Word(invalidValue) }
+                .message shouldBe "단어는 다섯글자로 구성됩니다"
+        }
+    }
+})
+
+class WordCompareTest: BehaviorSpec({
+
+    Given("'apple'이 단어로 주어졌을 때") {
+        val answer = Word("apple")
+
+        When("'widow'로 비교하면") {
+            val guess = Word("widow")
+            val result = answer.compare(guess)
+
+            Then("결과로 NONE, NONE, NONE, NONE, NONE 이 나온다") {
+                result shouldBe listOf(Compared.NONE, Compared.NONE, Compared.NONE, Compared.NONE, Compared.NONE)
+            }
+        }
+
+        When("'awake'로 비교하면") {
+            val guess = Word("awake")
+            val result = answer.compare(guess)
+
+            Then("결과로 EQUAL, NONE, NONE, NONE, EQUAL 이 나온다") {
+                result shouldBe listOf(Compared.EQUAL, Compared.NONE, Compared.NONE, Compared.NONE, Compared.EQUAL)
+            }
+        }
+
+        When("'AWAKE'로 비교하면") {
+            val guess = Word("AWAKE")
+            val result = answer.compare(guess)
+
+            Then("결과로 EQUAL, NONE, NONE, NONE, EQUAL 이 나온다") {
+                result shouldBe listOf(Compared.EQUAL, Compared.NONE, Compared.NONE, Compared.NONE, Compared.EQUAL)
+            }
+        }
+
+        When("'pride'로 비교하면") {
+            val guess = Word("pride")
+            val result = answer.compare(guess)
+
+            Then("결과로 EXIST, NONE, NONE, NONE, EQUAL 이 나온다") {
+                result shouldBe listOf(Compared.EXIST, Compared.NONE, Compared.NONE, Compared.NONE, Compared.EQUAL)
+            }
+        }
+
+        When("'apple로 비교하면") {
+            val guess = Word("apple")
+            val result = answer.compare(guess)
+
+            Then("결과로 EQUAL, EQUAL, EQUAL, EQUAL, EQUAL 이 나온다") {
+                result shouldBe listOf(Compared.EQUAL, Compared.EQUAL, Compared.EQUAL, Compared.EQUAL, Compared.EQUAL)
+            }
+        }
+    }
+})
+
+class AlphabetsPoolTest: BehaviorSpec({
+
+    Given("has 메서드는") {
+        val apple = "apple"
+        val applePool = AlphabetsPool(apple.map { Alphabet(it) })
+
+        When("해당 알파벳이 키에 없으면") {
+            val z = Alphabet('z')
+
+            Then("false를 반환한다") {
+                applePool.has(z) shouldBe false
+            }
+        }
+
+        When("해당 알파벳이 키에 있고, 값이 1 이상이면") {
+            val a = Alphabet('a')
+            val p = Alphabet('p')
+
+            Then("true를 반환한다") {
+                applePool.has(a) shouldBe true
+                applePool.has(p) shouldBe true
+            }
+        }
+
+        When("해당 알파벳이 키에 있고, 값이 0 이하면") {
+            val a = Alphabet('a')
+            applePool.spend(a)
+
+            Then("false를 반환한다") {
+                applePool.has(a) shouldBe false
+            }
+        }
+    }
 })
 
 class AlphabetTest: StringSpec({
