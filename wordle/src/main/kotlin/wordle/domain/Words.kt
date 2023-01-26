86b52da2f077f01e4362faa9ad7d932fdab91510
hyeonic <dev.hyeonic@gmail.com>
2023-01-26 19:07:19 +0900

    refactor: words 예외 메시지 수정

--- wordle/src/main/kotlin/wordle/domain/Words.kt
+++ wordle/src/main/kotlin/wordle/domain/Words.kt
@@ -5,6 +5,6 @@ class Words(
 ) {
 
     fun validateNonExist(word: Word) {
-        require(value.contains(word)) { "사전에 존재하지 않는 단어입니다." }
+        require(value.contains(word)) { "[예외] 사전에 존재하지 않는 단어입니다." }
     }
 }
