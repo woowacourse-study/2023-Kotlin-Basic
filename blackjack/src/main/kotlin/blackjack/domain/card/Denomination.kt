44f1401423dc4a539bd089fbfd74e606c821ceb2
hyeonic <dev.hyeonic@gmail.com>
2023-02-12 14:59:48 +0900

    feat: Denomination, Suit 추가

---
+++ blackjack/src/main/kotlin/blackjack/domain/card/Denomination.kt
@@ -0,0 +1,22 @@
+package blackjack.domain.card
+
+enum class Denomination(
+    val score: Int,
+    val value: String,
+) {
+
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K")
+    ;
+}
