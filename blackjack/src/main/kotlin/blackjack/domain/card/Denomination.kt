8d4465b1eb7f2757f29ba379343d84ac409cf327
hyeonic <dev.hyeonic@gmail.com>
2023-02-13 23:32:18 +0900

    feat: Denomination score 합산 로직 추가

--- blackjack/src/main/kotlin/blackjack/domain/card/Denomination.kt
+++ blackjack/src/main/kotlin/blackjack/domain/card/Denomination.kt
@@ -1,5 +1,8 @@
 package blackjack.domain.card
 
+private const val BLACKJACK_SCORE = 21
+private const val BONUS_ACE_SCORE = 11
+
 enum class Denomination(
     val score: Int,
     val value: String,
@@ -19,4 +22,20 @@ enum class Denomination(
     QUEEN(10, "Q"),
     KING(10, "K")
     ;
+
+    operator fun plus(score: Int): Int {
+        if (this == ACE) {
+            return calculateAceScore(score)
+        }
+
+        return this.score + score
+    }
+
+    private fun calculateAceScore(score: Int): Int {
+        if ((score + BONUS_ACE_SCORE) > BLACKJACK_SCORE) {
+            return score + ACE.score
+        }
+
+        return score + BONUS_ACE_SCORE
+    }
 }
