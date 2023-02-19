8684899e94cb17c1fa9564dbae8c2f698c340a5f
hyeonic <dev.hyeonic@gmail.com>
2023-02-19 13:52:44 +0900

    test: BettingMoney 검증 추가

--- blackjack/src/main/kotlin/blackjack/domain/participant/BettingMoney.kt
+++ blackjack/src/main/kotlin/blackjack/domain/participant/BettingMoney.kt
@@ -1,18 +1,32 @@
 package blackjack.domain.participant
 
 import java.math.BigDecimal
-
-private val MINIMUM_MONEY: BigDecimal = BigDecimal.valueOf(1_000)
-private val ZERO_MONEY: BigDecimal = BigDecimal(0)
+import java.math.RoundingMode
 
 data class BettingMoney private constructor(
-    private val value: BigDecimal
+    val value: BigDecimal,
 ) {
 
-    init {
-        require(value >= MINIMUM_MONEY) { "배팅 금액은 1000원 이상입니다." }
-        require(value % MINIMUM_MONEY == ZERO_MONEY) { "배팅 금액은 1000원으로 나누어 떨어져야 합니다." }
+    operator fun times(percent: Double): BettingMoney {
+        val multiplied = BigDecimal.valueOf(percent)
+        val result: BigDecimal = value.multiply(multiplied)
+        return BettingMoney(result.setScale(0, RoundingMode.FLOOR))
     }
 
-    constructor(value: String) : this(BigDecimal(value))
+    operator fun plus(otherBettingMoney: BettingMoney): BettingMoney {
+        return BettingMoney(value.add(otherBettingMoney.value).setScale(0, RoundingMode.FLOOR))
+    }
+
+    companion object {
+        val ZERO: BettingMoney = BettingMoney(BigDecimal.ZERO)
+
+        private const val MONEY_LENGTH = 4
+        private const val MONEY_DIVIDE_STANDARD = "000"
+
+        fun of(value: String): BettingMoney {
+            require(value.length >= MONEY_LENGTH) { "배팅 금액은 1000원 이상입니다." }
+            require(value.endsWith(MONEY_DIVIDE_STANDARD)) { "배팅 금액은 1000원으로 나누어 떨어져야 합니다." }
+            return BettingMoney(BigDecimal(value))
+        }
+    }
 }
